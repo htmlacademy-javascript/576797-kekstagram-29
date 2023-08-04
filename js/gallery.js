c8ee183b91b6eeb5cbd9cf51de33a7ae75b03f48
alexander mokryak <a.mokryak87@gmail.com>
2023-08-04 17:39:54 +0300

    Add pictures sorting

--- js/gallery.js
+++ js/gallery.js
@@ -1,16 +1,29 @@
 import {renderPictures} from './picture.js';
 import {showModal} from './big-picture-modal.js';
+import {sortPhotoList} from './sort.js';
 
 const pictures = document.querySelector('.pictures');
 
-function renderGallery(photoList) {
-  renderPictures(photoList);
+function renderGallery(photoList, sortMethod = '') {
+  const pictureItems = document.querySelectorAll('.picture');
+  // при перерисовке картинок удалям старые если есть
+  if (pictureItems.length !== 0) {
+    pictureItems.forEach((el) => el.remove());
+  }
+
+  const newPhotoList = photoList.slice();
+  if (!sortMethod || sortMethod === 'default') {
+    renderPictures(newPhotoList);
+  } else {
+    renderPictures(sortPhotoList(newPhotoList, sortMethod));
+  }
+
   pictures.addEventListener('click', (evt) => {
     const picturesId = evt.target.closest('[data-picture-id]');
     if (!picturesId) {
       return;
     }
-    const picture = photoList.find((item) => item.id === Number(picturesId.dataset.pictureId));
+    const picture = newPhotoList.find((item) => item.id === Number(picturesId.dataset.pictureId));
     showModal(picture);
   });
 }
--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 import {getData} from './api.js';
-import {showAlert} from './util.js';
+import {showAlert, debounce} from './util.js';
 import {renderGallery} from './gallery.js';
-import './editor.js';
+import {filterSection, setShowFilter} from './sort.js';
 import {editor} from './editor.js';
 
 getData('fetch')
   .then((pictures) => {
     renderGallery(pictures);
+    filterSection.classList.remove('img-filters--inactive');
+    setShowFilter(debounce((sortMethod) => renderGallery(pictures, sortMethod)));
   })
   .catch(
     (err) => {
